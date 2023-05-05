c3790e90dbeb7e0e93ddf23ccf87f03cae03895b
Walter Radduso <w.radduso@gmail.com>
2023-05-05 10:01:38 -0300

    feat: add tests, add husky, update nextjs... all configured

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, '/src/styles')],
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react';
 
 function Text(): ReactElement {
-  return <p className="p-4 text-red-700 dark:text-white">Text</p>;
+  return <p className="dark:text-white dark:bg-black text-center text-red-700 bg-white">Text</p>;
 }
 
 export default Text;
