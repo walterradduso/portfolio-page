135fef3478327da5654d6346128231a0254134dd
Walter Radduso <w.radduso@gmail.com>
2023-05-21 18:17:08 -0300

    feat: update resume pdf filename, update link component and download resume component

--- src/components/DownloadResume/DownloadResume.tsx
+++ src/components/DownloadResume/DownloadResume.tsx
@@ -3,8 +3,9 @@ import { Link } from '../Link';
 function DownloadResume() {
   return (
     <Link
+      download
       className="w-52 border-dark py-3 text-center text-dark transition duration-300 ease-in-out hover:bg-dark hover:text-white dark:border-white dark:text-white hover:dark:bg-white hover:dark:text-dark"
-      href="/resume.pdf"
+      href="/resume-walter-radduso.pdf"
     >
       Download Resume
     </Link>
